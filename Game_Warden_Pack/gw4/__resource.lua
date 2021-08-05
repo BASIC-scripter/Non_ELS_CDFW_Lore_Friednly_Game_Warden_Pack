9761bfe498011a429f03506847cb49702d7c0da8
BASIC <85481790+BASIC-scripter@users.noreply.github.com>
2021-08-04 21:36:30 -0700

    Add files via upload

---
+++ Game_Warden_Pack/gw4/__resource.lua
@@ -0,0 +1,27 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+     'data/21F150/carcols.meta',
+     'data/21F150/carvariations.meta',
+     'data/21F150/handling.meta',
+     'data/21F150/vehicles.meta',
+     'data/21F150LB/carcols.meta',
+     'data/21F150LB/carvariations.meta',
+     'data/21F150LB/handling.meta',
+     'data/21F150LB/vehicles.meta',
+}
+
+
+--.\compiled\data\21F150
+data_file 'HANDLING_FILE' 'data/21F150/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/21F150/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/21F150/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/21F150/carvariations.meta'
+
+
+--.\compiled\data\21F150LB
+data_file 'HANDLING_FILE' 'data/21F150LB/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/21F150LB/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/21F150LB/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/21F150LB/carvariations.meta'
+--#EOF
